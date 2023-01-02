85c2b8f6bf120246395f59df9fbbb44734203b26
ArtMan0 <artur2006@o2.pl>
2023-01-02 21:00:03 +0100

    Fix and improvement 2

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -1,15 +1,19 @@
 #include "core.hpp"
 
-ParkingSpace::ParkingSpace(int id):
-    id_(id)
+ParkingSpace::ParkingSpace(int id, int owner_id, std::string location):
+    id_(id),   
+    owner_id_(owner_id),
+    location_(location)
 {}
 
-Offer::Offer(int id,int id_offered_parking_space):
+Offer::Offer(int id,int id_parking_space, std::string description):
     id_(id),
-    parking_space_id(id_offered_parking_space)
+    parking_space_id(id_parking_space),
+    description_(description)
 {}
 
-User::User(int id, std::string password): 
+User::User(int id, std::string username, std::string password):
     id_(id),
+    username_(username),
     password_(password)
 {}
--- src/core/core.hpp
+++ src/core/core.hpp
@@ -3,40 +3,40 @@
 
 
 /* USER INTERACTION */
-void MakeOffer()   {}
 void RemoveOffer() {}
 void RentOffer()   {}
-void BuyOffer()    {}
 
 
 class ParkingSpace{
 public:
-    ParkingSpace(int id);
+    ParkingSpace(int id, int owner_id, std::string location);
 
 private:
     const unsigned int id_;
+    const unsigned int owner_id_;
+    std::string location_;
+
 };
 
 class Offer{
 public:
-    Offer(int id,int id_parking_space);
+    Offer(int id,int id_parking_space, std::string description);
 
 protected:
     const unsigned int id_;
     const unsigned int parking_space_id;
+    std::string description_;
 
 };
 
 class User{
 public:
-    User(int id, std::string password);
+    User(int id, std::string username, std::string password);
 
 private:
     const unsigned int id_;
+    std::string username_;
     std::string password_;
-
-    std::vector<const unsigned int> id_owned_parking_spaces;
-    std::vector<const unsigned int> id_posted_offers;
 
 };
 
