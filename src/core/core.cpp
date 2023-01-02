055cf3f2277ab3ef799f3e87abe85e918b804dd4
ArtMan0 <artur2006@o2.pl>
2023-01-02 18:56:03 +0100

    a bit of functionality

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -1,11 +1,15 @@
 #include "core.hpp"
 
+void getAllData()
+{}
+
 ParkingSpace::ParkingSpace(int id):
     id_(id)
 {}
 
-Offer::Offer(int id):
-    id_(id)
+Offer::Offer(int id,int ID_of_Offered_Parking_Space):
+    id_(id),
+    ID_of_Offered_Parking_Space_(ID_of_Offered_Parking_Space)
 {}
 
 User::User(int id, string password): 
--- src/core/core.hpp
+++ src/core/core.hpp
@@ -1,5 +1,16 @@
 #include <string>
+#include <vector>
 using namespace std;
+
+
+std::vector<ParkingSpace> ParkingSpaces;
+std::vector<Offer> Offers;
+std::vector<User> Users;
+
+
+/* TAKES ALL DATA FROM DATABASE */
+void getAllData();
+
 
 class ParkingSpace{
 public:
@@ -11,19 +22,30 @@ private:
 
 class Offer{
 public:
-    Offer(int id);
+    Offer(int id,int ID_of_Offered_Parking_Space);
 
-private:
+protected:
     const unsigned int id_;
-    
+    const unsigned int ID_of_Offered_Parking_Space_;
+
 };
 
 class User{
 public:
     User(int id, string password);
 
+    /* USER INTERACTION */
+    void MakeOffer()   {};
+    void RemoveOffer() {};
+    void RentOffer()   {};
+    void BuyOffer()    {};
+
 private:
     const unsigned int id_;
     string password_;
+
+    std::vector<const unsigned int> ID_of_Owned_Parking_Spaces;
+    std::vector<const unsigned int> ID_of_Posted_Offers;
+
 };
 
