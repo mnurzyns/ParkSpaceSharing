13e76c519251754fcf4b8fcb5c24af9da77e6515
ArtMan0 <artur.2006@o2.pl>
2023-01-21 16:00:45 +0100

    core calases to struct

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -1,22 +1,4 @@
 #include "core.hpp"
 
-ParkingSpace::ParkingSpace(int id, int owner_id, std::string location):
-    id_(id),
-    owner_id_(owner_id),
-    location_(location)
-{}
-
-Offer::Offer(int id,int id_parking_space, std::string description):
-    id_(id),
-    parking_space_id(id_parking_space),
-    description_(description)
-{}
-
-//User::User(int id, std::string username, std::string password):
-//    id_(id),
-//    username_(username),
-//    password_(password)
-//{}
-
 void RemoveOffer() {}
 void RentOffer()   {}
