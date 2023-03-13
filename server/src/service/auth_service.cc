13e4684d6485fa7151c436df2bd8f1de333ccc72
ArtMan0 <artur.2006@o2.pl>
2023-03-13 07:09:00 +0100

    Tokens JWT

--- server/src/service/auth_service.cc
+++ server/src/service/auth_service.cc
@@ -7,35 +7,23 @@ using Status = ::oatpp::web::protocol::http::Status;
 namespace server::service
 {
 
-    ::oatpp::Object<::server::dto::auth_dto> auth_service::signUp(::oatpp::Object<::server::dto::signUp_dto> const& dto) {
+::oatpp::Object<::server::dto::auth_dto> auth_service::signUp(::oatpp::Object<::server::dto::signUp_dto> const& dto) {
 
         //Is already exist user with same username and email
         auto res = database_->is_exist(dto->email,dto->username);
         OATPP_ASSERT_HTTP(res->isSuccess(), Status::CODE_500, res->getErrorMessage());
-        
-        auto fetch_exist = res->fetch<::oatpp::Vector<::oatpp::UInt32>>();
-        OATPP_ASSERT_HTTP(fetch_exist->empty(), Status::CODE_409, "User already exists");
+        OATPP_ASSERT_HTTP(!res->hasMoreToFetch(), Status::CODE_409, "User already exists");
     
         //Adding user to database
         res = database_->signUp(dto);
         OATPP_ASSERT_HTTP(res->isSuccess(), Status::CODE_500, res->getErrorMessage());
         OATPP_ASSERT_HTTP(!res->hasMoreToFetch(), Status::CODE_404, "User not Created");
-
-        res = database_->signIn(dto->email,dto->password);
-        OATPP_ASSERT_HTTP(res->isSuccess(), Status::CODE_500, res->getErrorMessage());
-        OATPP_ASSERT_HTTP(res->hasMoreToFetch(), Status::CODE_404, "User not Created");
-
-        auto fetch_user = res->fetch<::oatpp::Vector<::oatpp::Object<::server::dto::user_dto>>>();
-        OATPP_ASSERT_HTTP(fetch_user->size() == 1, Status::CODE_500, "Unknown error");
-
-        //Generating tokn
-        auto payload = std::make_shared<JWT::Payload>();
-        payload->user = fetch_user[0];
-        auto auth = ::server::dto::auth_dto::createShared();
-        auth->token = jwt_->createToken(payload);
-        auth->admin = fetch_user[0]->admin;
         
-        return auth;
+        ::oatpp::Object<::server::dto::signIn_dto> user = std::make_shared<dto::signIn_dto>();
+        user->password = dto->password;
+        user->email = dto->email;
+        
+        return signIn(user);
     }
 
     ::oatpp::Object<::server::dto::auth_dto> auth_service::signIn(::oatpp::Object<::server::dto::signIn_dto> const& dto) {
