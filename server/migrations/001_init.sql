a00c7c25be1399985587e61dadbcec48fe79180b
ArtMan0 <artur.2006@o2.pl>
2023-04-11 12:32:31 +0200

    samples

--- server/migrations/001_init.sql
+++ server/migrations/001_init.sql
@@ -31,4 +31,21 @@ CREATE INDEX IF NOT EXISTS `idx_offer_parking_space_id`
     ON `offer`(`parking_space_id`);
 
 
-INSERT INTO user(username, email, password, admin) VALUES("admin", "admin@admin", "admin", 1);
+-- Insert sample users
+INSERT INTO user(username, email, password, admin) 
+VALUES
+("Bob", "bob@example.com", "passw1", 0),
+("Tom", "tom@example.com", "passw2", 0),
+("admin", "admin@admin.com", "admin", 1);
+
+-- Insert sample parking spaces
+INSERT INTO parking_space(owner_id, location)
+VALUES
+(1, "123 Main St"),
+(2, "456 Oak St");
+
+-- Insert sample offers
+INSERT INTO offer(parking_space_id, description)
+VALUES
+(1, "Reserved parking spot in a gated lot."),
+(2, "Covered parking spot in a garage.");
